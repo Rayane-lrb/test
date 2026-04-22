94017d4db33295b7ac0bcb9613b34c111f81ac6e
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-04-22 15:48:03 +0200

    FEATURE: contact controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+
