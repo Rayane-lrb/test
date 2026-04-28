0e1a19422cb292824f7e4604be91e6ddf589e536
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-04-28 09:48:11 +0200

    FEATURE: style changes

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -6,27 +6,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     @vite('resources/css/app.css')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4/dist/css/splide.min.css">
     <title>Document</title>
 </head>
-<body class="h-screen font-sans font-semibold">
+<body class="min-h font-sans font-semibold bg-cover bg-fixed bg-center" style="background-image: url('/images/jonatan-pie-EvKBHBGgaUo-unsplash.jpg') ">
 <header class="">
-    <nav class="bg-green-700 p-4 flex text-lg items-center justify-between">
-        <a href="/" class="text-white hover:text-green-200 transition font-bold">Home</a>
+    <nav class="relative z-10  backdrop-blur-xs p-4 flex text-lg m-5 rounded-lg items-center justify-between">
+        <a href="/" class="text-green-200 hover:text-white transition underline-offset-4 hover:underline font-bold">Home</a>
         <div class="flex gap-4">
-            <a href="/contact" class="text-white hover:text-green-200 transition font-bold">Contact</a>
-            <a href="/faq" class="text-white hover:text-green-200 transition font-bold">Faq</a>
+            <a href="/contact" class="text-green-200 hover:text-white underline-offset-4 hover:underline transition font-bold">Contact</a>
+            <a href="/faq" class="text-green-200 hover:text-white underline-offset-4 hover:underline transition font-bold">Faq</a>
             <div>
                 <a href="/login" class="bg-white text-green-400 px-4 py-2 font-semibold rounded hover:bg-green-100 transition">Login</a>
             </div>
         </div>
     </nav>
 </header>
-<main>
+<main class="flex-1">
     {{$slot}}
 </main>
-<footer class="bg-green-300 p-1 ">
+<footer class="mt-auto bg-green-300 p-1">
     <hr>
     <p>Copyright 2026</p>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4/dist/js/splide.min.js"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,32 @@
 <x-site-layout>
-    <h1>Welcome</h1>
+            <section class="p-6">
+                <div id="splide" class="splide relative">
+                    <div class="splide__track">
+                        <ul class="splide__list">
+                            <li class="splide__slide">
+                                <img src="/images/andrew-coelho-aL7SA1ASVdQ-unsplash.jpg" alt="slide 1" class="w-full h-96 object-cover rounded-lg">
+                            </li>
+                            <li class="splide__slide">
+                                <img src="/images/ash-amplifies-NQ6Lh81BTRs-unsplash.jpg" alt="slide 2" class="w-full h-96 object-cover rounded-lg">
+                            </li>
+                            <li class="splide__slide">
+                                <img src="/images/jeremy-cai-ucYWe5mzTMU-unsplash.jpg" alt="slide 3" class="w-full h-96 object-cover rounded-lg">
+                            </li>
+                        </ul>
+                        <button class="bg-amber-50 rounded-lg p-2">
+                            Explore
+                        </button>
+                    </div>
+                </div>
+            </section>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            new Splide('#splide', {
+                type: 'loop',
+                autoplay: true,
+                interval: 3000,
+                pauseOnHover: true,
+            }).mount();
+        });
+    </script>
 </x-site-layout>
